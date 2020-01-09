e99f23b6c20efb6f283754bd3b6df98c2d251dbf
MaFarnSch <39802013+MaFarnSch@users.noreply.github.com>
2020-01-09 16:13:25 +0100

    Run rustfmt

--- sophia/src/graph/adapter/_error.rs
+++ sophia/src/graph/adapter/_error.rs
@@ -10,14 +10,14 @@ use std::error::Error;
 /// [mutation error]: ../trait.MutableGraph.html#associatedtype.MutationError
 #[derive(Debug, thiserror::Error)]
 pub enum GraphAsDatasetError<MGE: 'static + Error> {
-	/// Raised when the [adapter] is requested to modify a triple in a named
-	/// `Graph` which is not supported as it is only a singel `Graph` wrapped.
-	///
-	/// [adapter]: ./struct.GraphAsDataset.html
-	///
-	#[error("GraphAsDataset does not support named graphs")]
-	GraphNamesNotSupported,
-	/// Error from the wrapped `Graph`
+    /// Raised when the [adapter] is requested to modify a triple in a named
+    /// `Graph` which is not supported as it is only a singel `Graph` wrapped.
+    ///
+    /// [adapter]: ./struct.GraphAsDataset.html
+    ///
+    #[error("GraphAsDataset does not support named graphs")]
+    GraphNamesNotSupported,
+    /// Error from the wrapped `Graph`
     #[error("{0}")]
     FromGraph(#[from] MGE),
 }
