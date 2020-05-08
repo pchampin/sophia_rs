c4da4a346a91d282afcd712f43fb4da15269591e
MattesWhite <dagda92@googlemail.com>
2020-05-08 12:43:48 +0200

    Remove unnecessary 'clone()'s

--- sophia/src/dataset/_isomorphism.rs
+++ sophia/src/dataset/_isomorphism.rs
@@ -143,7 +143,6 @@ where
             mapped.iter().any(|t| same_graph_name(Some(t), other))
         }) as _
     } else {
-        let g = g.clone();
         Box::new(move |other: Option<&RefTerm>| same_graph_name(g, other)) as _
     }
 }
@@ -190,7 +189,6 @@ where
     if let Some(Term::BNode(_)) = t {
         Box::new(move |_: Option<&RefTerm>| true) as _
     } else {
-        let t = t.clone();
         Box::new(move |other: Option<&RefTerm>| same_graph_name(t, other)) as _
     }
 }
