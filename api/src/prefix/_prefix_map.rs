8e31d329851c7cebf891e86610276ef86a15a9f5
Pierre-Antoine Champin <pchampin@liris.cnrs.fr>
2021-04-11 13:26:31 +0200

    refactor PrefixMap

--- api/src/prefix/_prefix_map.rs
+++ api/src/prefix/_prefix_map.rs
@@ -1,80 +1,143 @@
+use super::{AsPrefix, IsPrefix, Prefix};
 /// Define the [`PrefixMap`] trait with default implementation.
-use crate::term::{SimpleIri, TTerm, TermKind};
+use crate::term::{TTerm, TermKind};
 use mownstr::MownStr;
-use std::borrow::Borrow;
-use std::collections::HashMap;
-use std::hash::Hash;
+use sophia_iri::{AsIri, Iri, IsIri};
 
-/// A prefix map associates prefixes (`&'a str`) to namespaces.
-pub trait PrefixMap<'a> {
-    /// The type of term returned by this prefix map.
-    type Term: TTerm + 'a;
+/// A prefix map associates prefixes to namespaces.
+pub trait PrefixMap {
     /// Return the IRI associated to this prefix, if any.
-    ///
-    /// It must be guaranteed that the returned term is indeed an IRI.
-    fn get_namespace(&self, prefix: &str) -> Option<&Self::Term>;
+    fn get_namespace(&self, prefix: &str) -> Option<Iri>;
     /// Return a prefix-suffix pair describing the given IRI, if any.
     ///
     /// If `iri` is another kind of term, implementations MUST return None.
-    fn get_prefixed_pair<'s, T: TTerm>(&'s self, iri: &'s T) -> Option<(&'s str, MownStr<'s>)>;
+    fn get_prefixed_pair<'s, T: TTerm>(&'s self, iri: &'s T) -> Option<(Prefix, MownStr<'s>)> {
+        self.get_checked_prefixed_pair(iri, |_| true)
+    }
+    /// Return a prefix-suffix pair describing the given IRI, if any,
+    /// guaranteeing that the suffix will satisfy the given predicate.
+    ///
+    /// If `iri` is another kind of term, implementations MUST return None.
+    fn get_checked_prefixed_pair<'s, T, F>(
+        &'s self,
+        iri: &'s T,
+        suffix_check: F,
+    ) -> Option<(Prefix, MownStr<'s>)>
+    where
+        T: TTerm,
+        F: Fn(&str) -> bool;
 }
 
-impl<'a, PF> PrefixMap<'a> for HashMap<PF, SimpleIri<'a>>
+impl<P, N> PrefixMap for [(P, N)]
 where
-    PF: Borrow<str> + Eq + Hash + 'a,
+    P: IsPrefix,
+    N: IsIri,
 {
-    type Term = SimpleIri<'a>;
-
-    fn get_namespace(&self, prefix: &str) -> Option<&Self::Term> {
-        self.get(prefix)
-    }
-    fn get_prefixed_pair<'s, T: TTerm>(&'s self, iri: &'s T) -> Option<(&'s str, MownStr<'s>)> {
-        match iri.kind() {
-            TermKind::Iri => {
-                let raw_value = iri.value_raw();
-                let len = raw_value.len();
-                self.iter()
-                    .filter_map(|(prefix, ns)| {
-                        let ns = ns.value_raw();
-                        if raw_value.starts_with(ns.bytes()) {
-                            Some((prefix.borrow(), raw_value.slice(ns.len()..)))
-                        } else {
-                            None
-                        }
-                    })
-                    .max_by_key(|(_, suffix)| len - suffix.len())
+    fn get_namespace(&self, prefix: &str) -> Option<Iri> {
+        for (p, n) in self {
+            if p.borrow() == prefix {
+                return Some(n.as_iri());
             }
-            _ => None,
         }
+        None
+    }
+    fn get_checked_prefixed_pair<'s, T, F>(
+        &'s self,
+        iri: &'s T,
+        suffix_check: F,
+    ) -> Option<(Prefix, MownStr<'s>)>
+    where
+        T: TTerm,
+        F: Fn(&str) -> bool,
+    {
+        if iri.kind() != TermKind::Iri {
+            return None;
+        }
+        let iri_val = iri.value();
+        let mut matched = 0;
+        let mut found = None;
+        for (p, n) in self {
+            let n_str = n.borrow();
+            if iri_val.starts_with(n_str) && n_str.len() > matched {
+                let maybe_matched = n.borrow().len();
+                let suffix = iri.value_raw().slice(maybe_matched..);
+                if suffix_check(&suffix) {
+                    matched = maybe_matched;
+                    found = Some((p.as_prefix(), suffix));
+                }
+            }
+        }
+        found
     }
 }
 
 #[cfg(test)]
 mod test {
+    use super::super::Prefix;
     use super::*;
+    use crate::term::SimpleIri;
+    use sophia_iri::Iri;
     use test_case::test_case;
+
+    fn make_map() -> Vec<(Prefix<'static>, Iri<'static>)> {
+        vec![
+            (
+                Prefix::new_unchecked("s"),
+                Iri::new_unchecked("http://schema.org/"),
+            ),
+            (
+                Prefix::new_unchecked("a"),
+                Iri::new_unchecked("http://example.org/a/"),
+            ),
+            (
+                Prefix::new_unchecked("ab"),
+                Iri::new_unchecked("http://example.org/a/b#"),
+            ),
+            (
+                Prefix::new_unchecked(""),
+                Iri::new_unchecked("http://example.org/"),
+            ),
+        ]
+    }
+
+    #[test_case("s", Some("http://schema.org/"); "s")]
+    #[test_case("a", Some("http://example.org/a/"); "a")]
+    #[test_case("ab", Some("http://example.org/a/b#"); "ab")]
+    #[test_case("", Some("http://example.org/"); "empty")]
+    #[test_case("sa", None; "sa")]
+    fn get_namespace(prefix: &str, expected: Option<&str>) {
+        let expected = expected.map(Iri::new_unchecked);
+        let map = make_map();
+        let got = map.get_namespace(prefix);
+        assert_eq!(got, expected);
+    }
 
     #[test_case("http://something.else.com/", None, None; "something else")]
     #[test_case("http://schema.org/Person", None, Some(("s", "Person")); "s:Person")]
     #[test_case("http://example.org/", None, Some(("", "")); "single colon")]
-    #[test_case("http://example.org/a/c", None, Some(("a", "c")); "a:c")]
-    #[test_case("http://example.org/a/b#c", None, Some(("ab", "c")); "b:c")]
-    #[test_case("http://example.org/a#c", None, Some(("", "a#c")); ":a#c")]
+    #[test_case("http://example.org/", Some("a/c"), Some(("a", "c")); "a:c")]
+    #[test_case("http://example.org/", Some("a/b#c"), Some(("ab", "c")); "b:c")]
+    #[test_case("http://example.org/a#", Some("c"), Some(("", "a#c")); ":a#c")]
     fn get_prefixed_pair(ns: &str, sf: Option<&str>, expected: Option<(&str, &str)>) {
-        let mut map = HashMap::new();
-        map.insert("s", SimpleIri::new_unchecked("http://schema.org/", None));
-        map.insert(
-            "ab",
-            SimpleIri::new_unchecked("http://example.org/", Some("a/b#")),
-        );
-        map.insert(
-            "a",
-            SimpleIri::new_unchecked("http://example.org/", Some("a/")),
-        );
-        map.insert("", SimpleIri::new_unchecked("http://example.org/", None));
-
-        let expected = expected.map(|(pf, sf)| (pf, MownStr::from(sf)));
+        let expected = expected.map(|(pf, sf)| (Prefix::new_unchecked(pf), MownStr::from(sf)));
+        let map = make_map();
         let iri = SimpleIri::new_unchecked(ns, sf);
-        assert_eq!(map.get_prefixed_pair(&iri), expected);
+        let got = map.get_prefixed_pair(&iri);
+        assert_eq!(got, expected);
+    }
+
+    #[test_case("http://something.else.com/", None, None; "something else")]
+    #[test_case("http://schema.org/Person", None, Some(("s", "Person")); "s:Person")]
+    #[test_case("http://example.org/", None, Some(("", "")); "single colon")]
+    #[test_case("http://example.org/", Some("a/c"), Some(("a", "c")); "a:c")]
+    #[test_case("http://example.org/", Some("a/b#c"), Some(("ab", "c")); "b:c")]
+    #[test_case("http://example.org/a#", Some("c"), None; ":a#c")]
+    fn get_checked_prefixed_pair(ns: &str, sf: Option<&str>, expected: Option<(&str, &str)>) {
+        let expected = expected.map(|(pf, sf)| (Prefix::new_unchecked(pf), MownStr::from(sf)));
+        let map = make_map();
+        let iri = SimpleIri::new_unchecked(ns, sf);
+        let suffix_check = |txt: &str| txt.chars().all(|c| c.is_alphabetic());
+        let got = map.get_checked_prefixed_pair(&iri, suffix_check);
+        assert_eq!(got, expected);
     }
 }
