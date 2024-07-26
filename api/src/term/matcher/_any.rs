7dd61a5a1571ccaea8ddd02fa5d6f5bbcd317a20
Mikhail Katychev <mkatych@gmail.com>
2024-07-26 15:55:35 -0500

    introduce Not term

--- api/src/term/matcher/_any.rs
+++ api/src/term/matcher/_any.rs
@@ -11,3 +11,14 @@ impl TermMatcher for Any {
         true
     }
 }
+
+/// Matches on the inverse of the inner [`Term`] or [`GraphName`]
+pub struct Not<M>(pub M);
+
+impl<M: TermMatcher> TermMatcher for Not<M> {
+    type Term = SimpleTerm<'static>; // not actually used
+
+    fn matches<T2: Term + ?Sized>(&self, term: &T2) -> bool {
+        !self.0.matches(term)
+    }
+}
