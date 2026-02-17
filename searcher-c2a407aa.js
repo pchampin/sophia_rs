c96ebba0949f6c66ac527b2a9fb2267b0aa245b4
pchampin <pchampin@users.noreply.github.com>
2026-02-17 20:02:37 +0000

    deploy: 98d71e9a83bb9f5eea328e24f5279af7031a3737

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-749fc0ac.js',
+                path_to_root + 'searchindex-5e70f649.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
